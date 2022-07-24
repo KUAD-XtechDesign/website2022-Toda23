0bcb512f41fd59b02fcbe82989024a0076103c62
k-matsumoto-kua <k-matsumoto@kua.kyoto-art.ac.jp>
2022-07-24 23:32:25 +0900

    コメント追加

--- js/main.js
+++ js/main.js
@@ -4,16 +4,19 @@ $(function(){//おまじない
     $("body").toggleClass('open_menu');
   })
 
-
+  /*以下簡易版マウスストーカー*/
+  //bodyにdiv要素(idはmousepoint)を追加。(中にidがcircleのdivがネストされている)
   $("body").append('<div id="mousepoint"><div id="circle"></div></div>')
   const mousepoint = "#mousepoint";
-  // 追従用の処理
+  
+  // マウスが動いた時にマウス座標と同じ位置に来るよう、直接CSSを指定
   $(window).mousemove(function(e){
     let x = e.clientX;
     let y = e.clientY;
     $(mousepoint).css({"transform": "translate(" + x + "px," + y + "px)"});
   });
 
+  //リンク(aタグ)に重なったときはahoverというクラスを付与
   $("a").hover(
     function(){$(mousepoint).addClass("ahover")},
     function(){$(mousepoint).removeClass("ahover")}
