82d26bf090b22b7f776ce6c1566a275877cc0563
k-matsumoto-kua <k-matsumoto@kua.kyoto-art.ac.jp>
2022-07-24 23:22:28 +0900

    簡易版マウスストーカー追加

--- js/main.js
+++ js/main.js
@@ -4,4 +4,19 @@ $(function(){//おまじない
     $("body").toggleClass('open_menu');
   })
 
+
+  $("body").append('<div id="mousepoint"><div id="circle"></div></div>')
+  const mousepoint = "#mousepoint";
+  // 追従用の処理
+  $(window).mousemove(function(e){
+    let x = e.clientX;
+    let y = e.clientY;
+    $(mousepoint).css({"transform": "translate(" + x + "px," + y + "px)"});
+  });
+
+  $("a").hover(
+    function(){$(mousepoint).addClass("ahover")},
+    function(){$(mousepoint).removeClass("ahover")}
+  )
+
 });//おまじない
